6fe50bfba316ef319814979d72a2b14980dee354
amin10003 <maxamedxasancali10003@gmail.com>
2026-06-25 11:30:02 +0300

    single trip deletion and auto-clear form input

--- ui/ui.py
+++ ui/ui.py
@@ -11,101 +11,187 @@ class TravelAssistantUI:
 
         self.window = ctk.CTk()
 
-        self.window.title("Smart Travel Assistant")
+        self.window.title(
+            "Smart Travel Assistant"
+        )
 
-        self.window.geometry("800x800")
+        self.window.geometry(
+            "800x850"
+        )
 
         self.build_ui()
 
     def build_ui(self):
 
         title = ctk.CTkLabel(
-            self.window, text="Smart Travel Assistant", font=("Arial", 28)
+            self.window,
+            text="Smart Travel Assistant",
+            font=("Arial", 28)
         )
 
         title.pack(pady=20)
 
         self.destination = ctk.CTkEntry(
-            self.window, placeholder_text="Destination", width=250
+            self.window,
+            placeholder_text="Destination",
+            width=250
         )
 
         self.destination.pack(pady=8)
 
-        self.date = ctk.CTkEntry(self.window, placeholder_text="Travel Date", width=250)
+        self.date = ctk.CTkEntry(
+            self.window,
+            placeholder_text="Travel Date",
+            width=250
+        )
 
         self.date.pack(pady=8)
 
-        self.notes = ctk.CTkEntry(self.window, placeholder_text="Notes", width=250)
+        self.notes = ctk.CTkEntry(
+            self.window,
+            placeholder_text="Notes",
+            width=250
+        )
 
         self.notes.pack(pady=8)
 
         self.distance = ctk.CTkEntry(
-            self.window, placeholder_text="Distance (km)", width=250
+            self.window,
+            placeholder_text="Distance (km)",
+            width=250
         )
 
         self.distance.pack(pady=8)
 
         self.vehicle = ctk.CTkOptionMenu(
-            self.window, values=["Bus", "Van", "Taxi"], width=250
+            self.window,
+            values=[
+                "Bus",
+                "Van",
+                "Taxi"
+            ]
         )
 
         self.vehicle.pack(pady=10)
 
+        self.trip_id = ctk.CTkEntry(
+            self.window,
+            placeholder_text="Trip ID to Delete",
+            width=250
+        )
+
+        self.trip_id.pack(pady=8)
+
         create_button = ctk.CTkButton(
-            self.window, text="Create Trip", command=self.create_trip
+            self.window,
+            text="Create Trip",
+            command=self.create_trip
         )
 
         create_button.pack(pady=5)
 
         history_button = ctk.CTkButton(
-            self.window, text="Show History", command=self.show_history
+            self.window,
+            text="Show History",
+            command=self.show_history
         )
 
         history_button.pack(pady=5)
 
+        delete_button = ctk.CTkButton(
+            self.window,
+            text="Delete One Trip",
+            command=self.delete_trip
+        )
+
+        delete_button.pack(pady=5)
+
         clear_button = ctk.CTkButton(
-            self.window, text="Clear Plans", command=self.clear_history
+            self.window,
+            text="Clear All Plans",
+            command=self.clear_history
         )
 
         clear_button.pack(pady=5)
 
-        self.output = ctk.CTkTextbox(self.window, width=650, height=300)
+        self.output = ctk.CTkTextbox(
+            self.window,
+            width=650,
+            height=300
+        )
 
         self.output.pack(pady=20)
+
+    def clear_inputs(self):
+
+        self.destination.delete(0, "end")
+
+        self.date.delete(0, "end")
+
+        self.notes.delete(0, "end")
+
+        self.distance.delete(0, "end")
 
     def create_trip(self):
 
         try:
 
             trip = self.planner.create_trip(
+
                 self.destination.get(),
+
                 self.date.get(),
+
                 self.notes.get(),
-                float(self.distance.get()),
-                self.vehicle.get(),
+
+                float(
+                    self.distance.get()
+                ),
+
+                self.vehicle.get()
             )
 
-            self.output.delete("1.0", "end")
+            self.output.delete(
+                "1.0",
+                "end"
+            )
 
-            self.output.insert("end", "Trip Created\n\n")
+            self.output.insert(
 
-            self.output.insert("end", trip.display_trip())
+                "end",
+
+                trip.display_trip()
+            )
+
+            self.clear_inputs()
 
         except Exception as error:
 
-            self.output.delete("1.0", "end")
+            self.output.delete(
+                "1.0",
+                "end"
+            )
 
-            self.output.insert("end", str(error))
+            self.output.insert(
+                "end",
+                str(error)
+            )
 
     def show_history(self):
 
         trips = self.planner.history()
 
-        self.output.delete("1.0", "end")
+        self.output.delete(
+            "1.0",
+            "end"
+        )
 
         if not trips:
 
-            self.output.insert("end", "No saved trips.")
+            self.output.insert(
+                "end",
+                "No saved trips."
+            )
 
             return
 
@@ -113,6 +199,7 @@ class TravelAssistantUI:
 
             self.output.insert(
                 "end",
+
                 f"""
 ID: {row[0]}
 Destination: {row[1]}
@@ -123,16 +210,53 @@ Notes: {row[5]}
 
 --------------------
 
-""",
+"""
+            )
+
+    def delete_trip(self):
+
+        try:
+
+            trip_id = int(
+                self.trip_id.get()
+            )
+
+            self.planner.delete_trip(
+                trip_id
+            )
+
+            self.trip_id.delete(
+                0,
+                "end"
+            )
+
+            self.show_history()
+
+        except:
+
+            self.output.delete(
+                "1.0",
+                "end"
+            )
+
+            self.output.insert(
+                "end",
+                "Enter valid Trip ID"
             )
 
     def clear_history(self):
 
         self.planner.clear_history()
 
-        self.output.delete("1.0", "end")
+        self.output.delete(
+            "1.0",
+            "end"
+        )
 
-        self.output.insert("end", "All plans deleted.")
+        self.output.insert(
+            "end",
+            "All plans deleted."
+        )
 
     def run(self):
 
