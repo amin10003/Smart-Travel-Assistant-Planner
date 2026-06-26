a1f917833d2602e6a3f5868f9b48961c12d5e7a7
amin10003 <maxamedxasancali10003@gmail.com>
2026-06-26 09:17:43 +0300

    update vehicle rate

--- services/fare_services.py
+++ services/fare_services.py
@@ -2,36 +2,19 @@ class FareService:
 
     def __init__(self):
 
-        self.vehicle_rates = {
-            "Bus": 30,
-            "Van": 50,
-            "Taxi": 150
-        }
+        self.vehicle_rates = {"Bus": 20, "Van": 30, "Taxi": 50}
 
-    def estimate_fare(
-        self,
-        distance,
-        vehicle
-    ):
+    def estimate_fare(self, distance, vehicle):
 
         if vehicle not in self.vehicle_rates:
 
-            return {
-                "success": False,
-                "message": "Vehicle not supported"
-            }
+            return {"success": False, "message": "Vehicle not supported"}
 
-        fare = (
-            distance
-            *
-            self.vehicle_rates[
-                vehicle
-            ]
-        )
+        fare = distance * self.vehicle_rates[vehicle]
 
         return {
             "success": True,
             "vehicle": vehicle,
             "distance": distance,
-            "estimated_fare": fare
+            "estimated_fare": fare,
         }
